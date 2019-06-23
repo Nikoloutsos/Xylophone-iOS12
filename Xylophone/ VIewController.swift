6436d54f858e863e482c9ea19e1c35fc3191baf6
Konstantinos Nikoloutsos <konstantinosnikolouts@gmail.com>
2019-06-23 18:35:20 +0300

    add solution

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,21 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
+    
+    var audioPlayer : AVAudioPlayer!
+    
+    let musicFormat = "wav"
+    let musicFileNames = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    
     }
 
 
-
+    //Click event handler
     @IBAction func notePressed(_ sender: UIButton) {
+        //Which button was clicked?(Fetch its TAG)
+        let tagBtn = sender.tag
         
+        //Find local Sound Url
+        let soundURL = Bundle.main.url(forResource: musicFileNames[tagBtn - 1], withExtension: musicFormat)
         
+        do{
+            //Create an audio player connected to the url.
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+            audioPlayer.play()
+        }catch{
+            //Hmm, something went wrong.
+            print(error)
+        }
         
     }
+    
     
   
 
